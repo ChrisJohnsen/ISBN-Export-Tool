fc83c3ae56f4e48d53aece7b7c71d36106ae390d
Chris Johnsen <chris_johnsen@pobox.com>
2022-12-07 05:47:04 -0600

    stop offering multiple entry points in utils
    Having them basically just doubles rollup times since they have to
    processed twice (once as part of index, once separately). If they were
    different packages altogether this wouldn't be a happen (although they
    wouldn't share rollup startup time then...).

--- packages/node/src/goodreads-tool.ts
+++ packages/node/src/goodreads-tool.ts
@@ -1,7 +1,7 @@
 import { readFile } from 'node:fs/promises';
 import { Builtins, Cli, Command, Option } from 'clipanion';
-import { collect, pipe, flatPipe, filter, prop, eq, map, pick, not } from 'utils/functional';
-import { reduceCSV, unparse } from 'utils/csv';
+import { collect, pipe, flatPipe, filter, prop, eq, map, pick, not } from 'utils';
+import { reduceCSV, unparse } from 'utils';
 
 class MissingISBNs extends Command {
   static usage = Command.Usage({
--- packages/utils/rollup.config.js
+++ packages/utils/rollup.config.js
@@ -9,22 +9,6 @@ const externalPlugins = [commonjs(), node_resolve(), ...basePlugins];
 
 export default [
   {
-    input: 'src/functional.ts',
-    output: [
-      { file: 'dist/functional.js' },
-      { file: 'dist/functional.cjs', format: 'cjs' },
-    ],
-    plugins: basePlugins,
-  },
-  {
-    input: 'src/csv.ts',
-    output: [
-      { file: 'dist/csv.js' },
-      { file: 'dist/csv.cjs', format: 'cjs' },
-    ],
-    plugins: externalPlugins,
-  },
-  {
     input: 'src/index.ts',
     output: [
       { file: 'dist/index.js' },
