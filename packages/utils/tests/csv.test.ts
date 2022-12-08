826b47eaa1bc7977ba508c8583bf958fd5928f48
Chris Johnsen <chris_johnsen@pobox.com>
2022-12-08 06:12:31 -0600

    test: reduceCSV: sum instead of array concat
    This shows that it is reducing, not just mapping.

--- packages/utils/tests/csv.test.ts
+++ packages/utils/tests/csv.test.ts
@@ -3,12 +3,13 @@ import { reduceCSV, type Row } from 'utils';
 import { outdent } from 'outdent';
 
 describe('reduceCSV', () => {
+  const csv = outdent`
+    a,b,c c,d
+    1,-2,three,04
+    2,"two words","=""quotes""",k
+  `;
+
   test('collect in array', async () => {
-    const csv = outdent`
-      a,b,c c,d
-      1,-2,three,04
-      2,"two words","=""quotes""",k
-    `;
     const result = await reduceCSV(csv, {
       fn: (acc, row) => acc.concat([row]),
       initial: [] as Row[]
@@ -17,5 +18,14 @@ describe('reduceCSV', () => {
     expect(result).toStrictEqual([
       { a: '1', b: '-2', 'c c': 'three', d: '04' },
       { a: '2', b: 'two words', 'c c': '="quotes"', d: 'k' }]);
+  });
+
+  test('sum a column', async () => {
+    const result = await reduceCSV(csv, {
+      fn: (acc, row) => acc + parseInt(row.a),
+      initial: 0
+    });
+
+    expect(result).toBe(3);
   });
 });
