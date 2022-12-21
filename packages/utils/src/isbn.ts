ad76edcfc04c76c0baaed5f557ed4264dbc718d6
Chris Johnsen <chris_johnsen@pobox.com>
2022-12-21 02:35:41 -0600

    isbn: some documentation

--- packages/utils/src/isbn.ts
+++ packages/utils/src/isbn.ts
@@ -12,6 +12,17 @@ export interface EditionsISBNResults {
   editionsFaults: ContentError[],
 }
 
+/**
+ * Fetch from Open Library (openlibrary.org) the ISBNs of all editions of the
+ * given ISBN.
+ *
+ * The result ISBNs are not validated (e.g. with validateISBN), but they are
+ * de-duplicated since they are returned in a Set<string>.
+ *
+ * Errors encountered while fetching or processing the initial ISBN-based query
+ * are reported in `workFaults`. Errors encountered while fetching or processing
+ * editions pages are reported in `editionsFaults`.
+ */
 export function otherEditionsOfISBN(fetch: Fetcher, isbn: string): Promise<EditionsISBNResults>;
 export function otherEditionsOfISBN(fetch: Fetcher): (isbn: string) => Promise<EditionsISBNResults>;
 export function otherEditionsOfISBN(fetch: Fetcher, isbn?: string): Promise<EditionsISBNResults> | ((isbn: string) => Promise<EditionsISBNResults>) {
@@ -216,10 +227,32 @@ function isObject<K extends PropertyKey>
 /// <reference path='./isbn3.d.ts'/>
 import { parse } from 'isbn3';
 
+/**
+ * Returns true if the given string is a valid ISBN.
+ *
+ * In addition to verifying the check digit, this will also verify whether the
+ * ISBN is a part of a currently-defined ISBN group range. This means that some
+ * numbers with otherwise correct check digits may be rejected.
+ */
 export function validateISBN(maybeISBN: string): boolean {
   return !!parse(maybeISBN);
 }
 
+/**
+ * If the given string is a valid ISBN (i.e. as per `validateISBN`), return all
+ * equivalent ISBNs (ISBN-13 and, if applicable, ISBN-10).
+ *
+ * Given a valid ISBN-10, return it and its 978-prefixed ISBN-13 equivalent.
+ *
+ * Given a valid 978-prefixed ISBN-13, return it and its ISBN-10 equivalent.
+ *
+ * Given a valid non-978-prefixed ISBN-13, return just it.
+ *
+ * Returned values are the non-hyphenated versions of the ISBN.
+ *
+ * If the given string is not a valid ISBN, return just a "normalized" version
+ * of the string (stripped of spaces and hyphens).
+ */
 export function equivalentISBNs(isbn: string): string[] {
   const validISBN = parse(isbn);
   if (!validISBN) return [isbn.replace(/\s|-/g, '')];
