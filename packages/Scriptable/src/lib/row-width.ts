1c18441a2f18343fd0221db55434fcb29f3749c9
Chris Johnsen <chris_johnsen@pobox.com>
2023-07-11 02:48:30 -0500

    apportionWidth: don't round weights
    Demonstrated the zero widthWeight issue in the "check" app, so the
    comment here can be deleted.
    
    I tested some non-integer widthWeights, but didn't run into any
    problems. The (unpublished, squashed) development history doesn't offer
    any hints: the rounding was in place when it was first committed. So
    remove the rounding until there is a documented reason to put it back.

--- packages/Scriptable/src/lib/row-width.ts
+++ packages/Scriptable/src/lib/row-width.ts
@@ -1,19 +1,9 @@
 export function apportionWidth<T extends Record<'widthWeight', number>>(things: readonly T[], width: number, cellSpacing = 2): (T & { width: number })[] {
-  // XXX (rounds to) zero widthWeight is inconsistent? It is possible to sneak
-  // in a zero widthWeight cell at the end of the row without affecting
-  // available (image) width. This "extra, zero-widthWeight cell at the end"
-  // works in landscape too (does not affect available image width), where it
-  // gets a bit more display width. A zero widthWeight at the beginning takes up
-  // nearly the whole row (actually, seems to be everything except the part that
-  // a zero widthWeight cell at the end can use?).
-
-  // It seems that widthWeights are rounded before being summed or divided
-  // inside Scriptable.
-  const rounded = things.map(thing =>
-    ({ ...thing, width: Math.round(thing.widthWeight) }));
-  const totalWeight = rounded.reduce((t, c) => t + c.width, 0);
+  const totalWeight = things.reduce((t, c) => t + Math.abs(c.widthWeight) || NaN, 0);
+  if (!isFinite(totalWeight) || totalWeight == 0)
+    throw new RangeError('each apportioned widthWeight must be finite and non-zero');
   if (cellSpacing)
     width -= cellSpacing * (things.length - 1);
-  return rounded.map(thing =>
-    ({ ...thing, width: Math.round(thing.width / totalWeight * width) })); // total might not quite equal width!
+  return things.map(thing =>
+    ({ ...thing, width: Math.round(Math.abs(thing.widthWeight) / totalWeight * width) })); // total might not quite equal width!
 }
